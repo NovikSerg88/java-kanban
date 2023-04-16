a59b4d6d7f30c883f299b80794aeceb4dbcd33f5
Sergey Novikov <NovikS88@yandex.ru>
2023-04-16 18:49:11 +1000

    feature:проект без тестов сервера

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+//Проект был случайно удален и восстановлен из папки out. Прошу рассмотреть проект без части тестов,
+// т.к. он в процессе восстановления
         new KVServer().start();
         new HttpTaskServer();
     }
